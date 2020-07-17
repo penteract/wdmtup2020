25185d209cf3c5759c603db56ff610eb85f78eeb
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2020-07-17 18:56:49 +0100

    Finish and fix serialization and deserialization.

--- app/Data.hs
+++ app/Data.hs
@@ -1,33 +1,44 @@
 module Data (
 ) where
 
-data Value = VFunction (Value -> Value) | VInt Integer | VCons Value Value | VNil | Picture --I'll deal with actual pciture data later.
+data Value = VFunction (Value -> Value) | VInt Integer | VCons Value Value | VNil | VPicture --I'll deal with actual pciture data later.
 
-serialize :: Value -> [Boolean]
-serialize (VList (x : xs)) = [True,True] ++ serialize x ++ serialize xs
-serialize (VList []) = [False,False]
-serialize (VInt n) = (if n >= 0 then [False, True] else [True, False]) ++ replicate bitLength True ++ [False] ++ replicate (nybbleLength * 4 - length bits) 0 ++ bits
+instance Show Value where
+    show (VFunction x) = "Fun"
+    show (VInt n) = show n
+    show (VCons x y) = "("++show x++", "++show y++")"
+    show VNil = "n"
+    show VPicture = "picture"
+
+serialize :: Value -> [Bool]
+serialize (VCons x xs) = [True,True] ++ serialize x ++ serialize xs
+serialize (VNil) = [False,False]
+serialize (VInt n) = (if n >= 0 then [False, True] else [True, False]) ++ replicate nybbleLength True ++ [False] ++ replicate (nybbleLength * 4 - length bits) False ++ bits
     where bits = convertToBits (abs n)
           nybbleLength = div (length bits + 3) 4
+serialize _ = error "Serialization only available for integers, lists and nil"
 
-convertToBits :: Integer -> [Boolean]
+convertToBits :: Integer -> [Bool]
 convertToBits n = reverse $ convertToBits' n
     where convertToBits' 0 = []
-          convertToBits' m = (m % 2 == 0) : convertToBits' (div m 2)
+          convertToBits' m = (mod m 2 /= 0) : convertToBits' (div m 2)
 
-deserialize :: [Boolean] -> Value
-deserialize = snd $ deserialize'
-    where deserialize' :: [Boolean] -> ([Boolean], Value)
+deserialize :: [Bool] -> Value
+deserialize = snd . deserialize'
+    where deserialize' :: [Bool] -> ([Bool], Value)
           deserialize' (False:False:xs) = (xs, VNil)
           deserialize' (True:True:xs) = let {
                   (xs', v0) = deserialize' xs;
                   (xs'', v1) = deserialize' xs'
               } in (xs'', VCons v0 v1)
           deserialize' (False:True:xs) = let {
-                  (lengthString, False:xs') = break id xs
+                  (lengthString, False:xs') = break not xs;
                   (bits, xs'') = splitAt (length lengthString * 4) xs'
               } in (xs'', VInt (convertFromBits bits))
           deserialize' (True:False:xs) = let {
-                  (lengthString, False:xs') = break id xs
+                  (lengthString, False:xs') = break not xs;
                   (bits, xs'') = splitAt (length lengthString * 4) xs'
               } in (xs'', VInt (-convertFromBits bits))
+
+convertFromBits :: [Bool] -> Integer
+convertFromBits = foldl (\n b -> n*2 + if b then 1 else 0) 0
