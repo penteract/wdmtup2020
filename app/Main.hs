c3430b531343ed2a19f8e62e24b065fad160a260
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2020-07-17 20:01:59 +0100

    Attach deserializer to HTTP thing.

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,21 @@ import Network.HTTP.Simple
 import Data.ByteString.Lazy.UTF8 as BLU
 import Control.Exception
 
+import Data
+
 main = catch (
     do  
         args <- getArgs
-        putStrLn ("ServerUrl: " ++ args!!0 ++ "; PlayerKey: " ++ args!!1)
-        request' <- parseRequest ("POST " ++ (args!!0))
-        let request = setRequestBodyLBS (BLU.fromString (args!!1)) request'
-        response <- httpLBS request
-        let statuscode = show (getResponseStatusCode response)
-        case statuscode of
-            "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
-            _ -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
+        if Prelude.length args < 2 then
+            putStrLn "Arguments required: URL to send to (including key: https://icfpc2020-api.testkontur.ru/aliens/send?apiKey=7897f34898d14e438f654b62eb7f8673) and message to send."
+        else do
+            request' <- parseRequest ("POST " ++ (args!!0))
+            let request = setRequestBodyLBS (BLU.fromString (args!!1)) request'
+            response <- httpLBS request
+            let statuscode = show (getResponseStatusCode response)
+            case statuscode of
+                "200" -> putStrLn ("Server response: " ++ (show $ deserialize $ BLU.toString $ getResponseBody response))
+                _ -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
     ) handler
     where
         handler :: SomeException -> IO ()
